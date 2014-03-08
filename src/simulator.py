fa65ab95c0e1bec7bd26471f739a40d843e26642
c-w <clemens.wolff+git@gmail.com>
2014-03-08 22:04:13 +0000

    Implement Ricart-Agrawala mutex algorithm

--- src/simulator.py
+++ src/simulator.py
@@ -11,15 +11,22 @@ PrintAction = namedtuple('PrintAction', 'payload')
 MutexStartAction = namedtuple('MutexStartAction', '')
 MutexEndAction = namedtuple('MutexEndAction', '')
 
-Packet = namedtuple('Packet', 'sender destination payload timestamp')
+Packet = namedtuple('Packet', 'sender destination payload time')
+MutexReq = namedtuple('MutexReq', 'sender time')
+MutexAck = namedtuple('MutexAck', 'sender destination')
 
 
 class Process(object):
-    def __init__(self, pid, actions, message_channel):
+    def __init__(self, pid, actions, network, message_channel, mutex_channel):
         self.pid = pid
         self.actions = actions
+        self.network = network
         self.message_channel = message_channel
+        self.mutex_channel = mutex_channel
         self.clock = 0
+        self.deferred_mutex_requests = set()
+        self.mutex_req = None
+        self.has_mutex = False
 
     @property
     def is_done(self):
@@ -30,10 +37,30 @@ class Process(object):
             pid=self.pid, actions=self.actions)
 
     def execute_next(self):
+        self.handle_mutex_requests()
         if self.is_done:
             return
         if self.handle_action(self.actions[0]):
             self.actions.popleft()
+
+    def lower_priority(self, mutex_req):
+        if self.clock > mutex_req.time:
+            return True
+        if self.clock < mutex_req.time:
+            return False
+        return self.pid > mutex_req.sender
+
+    def handle_mutex_requests(self):
+        reqs = (msg for msg in self.mutex_channel
+                if isinstance(msg, MutexReq) and msg.sender != self.pid)
+        acks = set()
+        for req in reqs:
+            ack = MutexAck(sender=self.pid, destination=req.sender)
+            if self.mutex_req is None or self.lower_priority(req):
+                acks.add(ack)
+            else:
+                self.deferred_mutex_requests.add(ack)
+        self.mutex_channel.update(acks)
 
     def handle_action(self, action):
         if isinstance(action, SendAction):
@@ -42,7 +69,7 @@ class Process(object):
                 sender=self.pid,
                 destination=action.destination,
                 payload=action.payload,
-                timestamp=self.clock)
+                time=self.clock)
             self.message_channel.append(packet)
             print 'sent {pid} {payload} {destination} {time}'.format(
                 pid=self.pid, payload=packet.payload,
@@ -58,7 +85,7 @@ class Process(object):
                     continue
                 if packet.payload != action.payload:
                     continue
-                self.clock = max(self.clock, packet.timestamp) + 1
+                self.clock = max(self.clock, packet.time) + 1
                 print 'received {pid} {payload} {sender} {time}'.format(
                     pid=self.pid, payload=packet.payload,
                     sender=packet.sender, time=self.clock)
@@ -68,29 +95,53 @@ class Process(object):
                 return False
 
         elif isinstance(action, PrintAction):
+            other_mutex = any(process.has_mutex for process in self.network)
+            if not self.has_mutex and other_mutex:
+                return False
             self.clock += 1
             print 'printed {pid} {payload} {time}'.format(
                 pid=self.pid, payload=action.payload, time=self.clock)
             return True
 
         elif isinstance(action, MutexStartAction):
-            raise NotImplementedError
+            if self.mutex_req is None:
+                self.mutex_req = MutexReq(sender=self.pid, time=self.clock)
+                self.mutex_channel.add(self.mutex_req)
+            acks = set(msg for msg in self.mutex_channel
+                       if isinstance(msg, MutexAck)
+                       and msg.destination == self.pid)
+            if len(acks) == len(network) - 1:
+                self.has_mutex = True
+                self.mutex_channel.remove(self.mutex_req)
+                return True
+            return False
 
         elif isinstance(action, MutexEndAction):
-            raise NotImplementedError
+            self.mutex_channel.update(self.deferred_mutex_requests)
+            self.mutex_req = None
+            self.has_mutex = False
+            self.deferred_mutex_requests = set()
+            return True
 
 
-def parse_input(infile, message_channel=None):
+def parse_input(infile, **kwargs):
+    network = kwargs.get('network', set())
+    message_channel = kwargs.get('message_channel', deque())
+    mutex_channel = kwargs.get('mutex_channel', set())
     process_name = None
     process_actions = deque()
-    message_channel = deque() if message_channel is None else message_channel
     lines = (line.lower().strip() for line in infile if line.strip())
     for lineno, line in enumerate(lines, start=1):
         tokens = line.split()
         if line.startswith('begin process'):
             process_name = tokens[2]
         elif line.startswith('end process'):
-            yield Process(process_name, process_actions, message_channel)
+            network.add(Process(
+                pid=process_name,
+                actions=process_actions,
+                network=network,
+                message_channel=message_channel,
+                mutex_channel=mutex_channel))
             process_name = None
             process_actions = deque()
         elif line.startswith('begin mutex'):
@@ -108,6 +159,7 @@ def parse_input(infile, message_channel=None):
                 'ignoring unknown command: "{command}" '
                 'on line {lineno} in file {filepath}'.format(
                     command=line, lineno=lineno, filepath=infile.name))
+    return network
 
 
 if __name__ == '__main__':
@@ -118,9 +170,9 @@ if __name__ == '__main__':
     infile = args.infile
 
     try:
-        processes = list(parse_input(infile))
-        while not all(process.is_done for process in processes):
-            for process in processes:
+        network = parse_input(infile)
+        while not all(process.is_done for process in network):
+            for process in network:
                 process.execute_next()
     finally:
         infile.close()
